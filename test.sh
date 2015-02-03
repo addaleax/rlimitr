a9656f5d95f1834f65a6910e7a10ca166d2329f3
Hauke Henningsen <sqrt@entless.org>
2015-02-03 19:16:59 +0100

    Do not use RLIMIT_STACK for testing since it varies between environments

--- test.sh
+++ test.sh
@@ -6,10 +6,9 @@ env RLIMIT_AS=50M ./rlimitr ./rlimitr | grep RLIMIT_AS | grep -Fq 0x3200000
 ./rlimitr RLIMIT_AS=50M ./rlimitr | grep RLIMIT_AS | grep -Fq 0x3200000
 ./rlimitr RLIMIT_AS=s50M:h50M ./rlimitr | grep RLIMIT_AS | grep -Fq s0x3200000:h0x3200000
 ./rlimitr RLIMIT_AS=RLIM_INFINITY ./rlimitr | grep RLIMIT_AS | grep -Fq RLIMIT_AS=sRLIM_INFINITY
-./rlimitr RLIMIT_STACK=+1 ./rlimitr | grep RLIMIT_STACK | grep -Fq 0001
-./rlimitr RLIMIT_STACK=-1 ./rlimitr | grep RLIMIT_STACK | grep -Fq ffff
-./rlimitr RLIMIT_STACK=+1K ./rlimitr | grep RLIMIT_STACK | grep -Fq 0400
-./rlimitr RLIMIT_STACK=-1K ./rlimitr | grep RLIMIT_STACK | grep -Fq fc00
+./rlimitr RLIMIT_NOFILE=+1 ./rlimitr | grep RLIMIT_NOFILE | grep -Fq 0x1
+./rlimitr RLIMIT_NOFILE=-1 ./rlimitr | grep RLIMIT_NOFILE | grep -Fq ff
+./rlimitr RLIMIT_AS=1048576K ./rlimitr | grep RLIMIT_AS | grep -Fq s0x40000000
 ./rlimitr RLIMIT_AS=1G ./rlimitr | grep RLIMIT_AS | grep -Fq s0x40000000
 ./rlimitr RLIMIT_AS=1T ./rlimitr | grep RLIMIT_AS | grep -Fq s0x10000000000
 ./rlimitr RLIMIT_AS=1P ./rlimitr | grep RLIMIT_AS | grep -Fq s0x4000000000000
